3814758daf09795149ddf4351a9326b09c9cb895
CodeKiro <kirofiniello@outlook.it>
2024-05-13 23:35:13 +0200

    the class for the 3 different type of artifcle have been created and implemented on the dom.

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ const containerEl = document.querySelector('div.container');
 
 for(let count=1; count <= 100; count++){
     let content = count;
+    const newArticle = document.createElement('article');
     if (count % 3 === 0 && count % 5 === 0){
         content = 'FizzBuzz';
+        newArticle.classList.add('bg-blu');
     }else if(count % 3 === 0){
         content = 'Fizz';
+        newArticle.classList.add('bg-green');
     }else if(count % 5 === 0){
         content = 'Buzz';
+        newArticle.classList.add('bg-red');
     }   
-    const newArticle = document.createElement('article');
     newArticle.append(content);
     containerEl.appendChild(newArticle);
     console.log(content);
