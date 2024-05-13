594d681de061d0e5845d29860417054184f801d6
CodeKiro <kirofiniello@outlook.it>
2024-05-13 22:58:01 +0200

    the const for the containerel have been created and tested  with a console.log

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const containerEl = document.querySelector('div.container');
+// console.log(containerEl);
+
 for(let count=1; count <= 100; count++){
     if (count % 3 === 0 && count % 5 === 0){
         console.log('FizzBuzz');
